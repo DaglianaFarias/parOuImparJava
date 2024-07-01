41a137ef7e8b8ecd77c11b1dbf882432217c5793
Dagliana Farias <dagliana.farias14@gmail.com>
2024-06-30 20:29:31 -0400

    Primeiro commit: Adicionando projeto ParOuImpar

---
+++ src/ParOuImpar.java
@@ -0,0 +1,25 @@
+// Importa a classe Scanner para ler a entrada do usuário
+import java.util.Scanner;
+
+// Define a classe ParOuImpar.
+public class ParOuImpar {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // Solicita ao usuário que digite um número.
+    System.out.print("Digite um número: ");
+
+    // Lê o número digitado pelo usuário.
+    int numero = scanner.nextInt();
+
+    // Verifica se o número é par ou ímpar e imprime a mensagem correspondente.
+    if (numero % 2 == 0) {
+        System.out.println("O número " + numero + " é par.");
+    } else {
+        System.out.println("O número " + numero + " é ímpar.");
+    }
+
+    // Fecha o objeto Scanner
+    scanner.close();
+  }
+}
